c75d8db0252907f1a691df14e2966e29593fc40c
Mohammad Imran <108978570+MrMi18@users.noreply.github.com>
2025-03-01 23:01:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 function trailingZeros(n) {
   //your JS code here. If required.
+	let fact =1 
+	while(n>0){
+		fact*=n;
+		n--;
+   }
+	fact = fact.toString();
+	let len = fact.length-1;
+	let count = 0;
+    for(let i  = len ; i>=0 ; i--){
+		if(fact[i]!=0) return count;
+		else count++;
+    }
+	return count;
 }
 
 const input = prompt("Enter a number");
+
 alert(trailingZeros(input));
